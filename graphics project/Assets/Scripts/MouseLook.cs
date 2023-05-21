cb867391b411a44c936e71f40e92ceb9478adf1b
smkuhl <83143997+smkuhl@users.noreply.github.com>
2023-05-21 00:27:29 -0700

    first person controller basics

---
+++ graphics project/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private float rotX = 0f;
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // MouseAiming();
+        // KeyboardMovement();
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        rotX -= mouseY;
+        rotX = Mathf.Clamp(rotX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotX, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        
+    }
+
+}
